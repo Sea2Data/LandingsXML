b49394561cc36445a0dc2e9346a0d0ff2aab950d
edvinf <edvin.fuglebakk@gmail.com>
2017-06-28 15:43:06 +0200

    Notater for konvertering

---
+++ src/main/java/imr/fd/ef/fdirformats/XMLConverter.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imr.fd.ef.fdirformats;
+
+/**
+ * Read LSS files and converts to xml
+ * @author Edvin Fuglebakk edvin.fuglebakk@imr.no
+ */
+public class XMLConverter {
+    
+    //read lss
+    //look up columns by name to make sure
+    // build jax objects
+    // write xml
+    
+}
