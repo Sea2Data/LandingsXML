62b80fd64ec17f348d19e1f5079ac50094b2443f
edvinf <edvin.fuglebakk@gmail.com>
2018-09-03 10:21:31 +0200

    populate sets init

---
+++ landingsets/populate_sets.R
@@ -0,0 +1,11 @@
+lls_files <- as.data.frame(year=list("2016", "2015"), path=list("", ""))
+target_location <- ""
+converter_location <- ""
+
+filter_and_dump <- function(species){
+  
+}
+
+convert <- function(){
+  
+}
